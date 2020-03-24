5687eb8400ddc65b4e20a81f49d6b46847a941fd
Focus <Pythonuni17@gmail.com>
2020-03-24 10:36:34 +0700

    Excerise4_Thitinan_A
    Dev

---
+++ Excercise4_Thitinan_A.py
@@ -0,0 +1,20 @@
+#CP3-Thitinan_Arthan
+#Excerise4_Thitinan_A.py
+
+#Foundation English = FE
+#Genneral Business = GB
+#introduction to Computer Systems = ICS
+#Computerfusgrogramming = CP
+
+FE = 89.0
+GB = 70.0
+ICS = 90.0
+CP = 100.0
+
+print("---Your Score---")
+print("Foundation English",FE)
+print("Genneral Business",GB)
+print("introduction to Computer Systems",ICS)
+print("Computer Programming",CP)
+
+
